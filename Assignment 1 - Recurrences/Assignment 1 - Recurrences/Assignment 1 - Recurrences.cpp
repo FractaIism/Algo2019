d3126019b0fd36842c5bcbc939ae99c24f0f5abc
FractaIism <fractalism.cs05@nctu.edu.tw>
2019-09-21 20:56:33 +0800

    recursive solution

--- Assignment 1 - Recurrences/Assignment 1 - Recurrences/Assignment 1 - Recurrences.cpp
+++ Assignment 1 - Recurrences/Assignment 1 - Recurrences/Assignment 1 - Recurrences.cpp
@@ -10,23 +10,32 @@ int main()
 	int k;
 	while (cin >> k) {
 		if (k == 0) return 0;
-		vector<int> S(k, 0), sel(1,1);
+		vector<int> S(k, 0), sel;
 		for (int i = 0; i < k; ++i) {
 			cin >> S[i];
 		}
 		GenerateSelection(S, sel);
+		cout << '\n';
 	} 
 	return 0;
 }
 
 void GenerateSelection(const vector<int>& S, vector<int> sel)
 {
+	int k = S.size();
 	if (sel.size() == 6) {
 		PrintSelection(S, sel);
 		return;
+	} else if (sel.empty()) { //initial
+		sel.push_back(0);
+		for (int i = 0; i <= k - 6; ++i) {
+			sel[0] = i;
+			GenerateSelection(S, sel);
+		}
+		return;
 	}
-	sel.push_back(sel.back() + 1);
-	for (int last = sel.back(); last < S.back(); ++last,++sel.back()) {
+
+	for (sel.push_back(sel.back()+1); sel.back() + (6 - sel.size()) <= k - 1; ++sel.back()) {
 		GenerateSelection(S, sel);
 	}
 	return;
@@ -35,7 +44,8 @@ void GenerateSelection(const vector<int>& S, vector<int> sel)
 void PrintSelection(const vector<int>& S, const vector<int>& sel)
 {
 	for (int i = 0; i < sel.size(); ++i) {
-		cout << S[i] << ' ';
+		cout << S[sel[i]] << ' ';
 	}
 	cout<<'\n';
+	return;
 }
